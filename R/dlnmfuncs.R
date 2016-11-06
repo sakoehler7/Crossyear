96669cd7c20a09055afb711028bbefa3547a6c30
sarahkoehler <sakoehler7@gmail.com>
2016-11-06 16:27:01 -0700

    Fix smoothrr, smoothlambda

--- R/dlnmfuncs.R
+++ R/dlnmfuncs.R
@@ -29,11 +29,57 @@ stratalambda <- function(exposure, baseline, rrvalues, lag, argvar, arglag){
 }
 
 #' 
+#' Continuous Relative Risk
 #' 
+#' This is an intermediate function to use while making the smooth rr custom 
+#' lambda function for eesim.
 #' 
+#' @examples 
+#' set.seed(15)
+#' testexpo <- sim_exposure(n=200, central = .01, amp = .01, exposure_type = "binary")
+#' testrr <- smoothrr(testexpo$x, lag = 20, scale = 6)
 #' 
+#' @export
 #' 
+smoothrr <- function(exposure, lag, scale){
+  #Create a vector that has a 1 on each day there was increased rr:
+  lagtime <- dlnm::crossbasis(x=exposure, lag = lag, argvar = list(fun = "thr", thr.value = 0))[,]
+  rrseq <- rep(0, length(lagtime))
+  for (i in 1:length(lagtime)){
+    if(is.na(lagtime[i])){
+      rrseq[i] <- 0
+    }
+  else if (lagtime[i]==1 & lagtime[i-1]==0){
+    rrseq[i:(i+lag-1)] = abs(-lag:-1)
+  }
+  else if (lagtime[i]==0){
+    rrseq[i]<-0
+  }
+}
+polylag <- dlnm:::poly(rrseq)/scale
+rr <- exp(polylag)
+return(rr)
+}
 #' 
+#' Smooth lambda
+#' 
+#' This function can be input as a custom lambda function in eesim.  It uses the
+#' smoothrr function to create lambda values based on smooth relative risks.
+#' 
+#' @examples 
+#' set.seed(15)
+#' testexpo <- sim_exposure(n=200, central = .01, amp = .01, exposure_type = "binary")
+#' base <- create_baseline(200, 58, trend = "no trend")
+#' smoothlambda(exposure=testexpo$x, baseline = base$baseline, lag = 20, scale = 2)
+#' 
+#' @export
+#' 
+smoothlambda <- function(exposure, baseline, lag, scale){
+  rr <- smoothrr(exposure, lag, scale)
+  log_lambda <- log(baseline)+log(rr)
+  lambda <- exp(log_lambda)
+  return(lambda)
+}
 #' 
 #' 
 #' 
